610612e3cb4a80b200b31172eecb7d8ccf895e20
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-06-14 21:39:34 -0400

    Before trying to solve NullPointException

--- src/chapter16/problem19/Help.java
+++ src/chapter16/problem19/Help.java
@@ -6,23 +6,57 @@
 package chapter16.problem19;
 
 import java.awt.FlowLayout;
+import java.util.HashMap;
+import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
 
-/*
-    Try This 4-1
-
-    Convert the help system from Try This 3-3 into
-    a Help class.
-*/
 
 class Help {
   JFrame jframeFrame;
   JList<String> jlistTopics;
-  JLabel jlabelSelectTopics, jlabel;
-  String[] arrayTopics = {"if", "switch", "for", "while", "do-while", "break", "continue"};
+  JLabel jlabelSelectTopics, jlabelTopicText;
+  static final String[] arrayTopics;
+  static final HashMap<String, String> hashTopicText;
+  
+  static {
+    arrayTopics = new String[] {"if", "switch", "for", "while", "do-while", "break", "continue"};
+    
+    hashTopicText = new HashMap<>();
+    
+    hashTopicText.put("if", "<html>The if:"
+            + "<br>if(condition) statement;"
+            + "<br>else statement;</html>");
+    
+    hashTopicText.put("switch", "<html>The switch:"
+            + "<br>switch(expression) {"
+            + "<br>  case constant:"
+            + "<br>    statement sequence"
+            + "<br>    break;"
+            + "<br>  // ..."
+            + "<br>}</html>");
+    
+    hashTopicText.put("for", "<html>The for:"
+            + "<br>for(init; condition; iteration)"
+            + "<br> statement;</html>");
+
+    hashTopicText.put("while", "<html>The while:"
+            + "<br>hile(condition) statement;</html>");
+
+    hashTopicText.put("do-while", "<html>The do-while:"
+            + "<br>do {"
+            + "<br>  statement;"
+            + "<br>} while (condition);</html>");
+    
+    hashTopicText.put("break", "<html>The break:"
+            + "<br>break; or break label;</html>");
+    
+    hashTopicText.put("continue", "<html>The continue:"
+            + "<br>continue; or continue label;</html>");
+  }
   
   Help()  {
     jframeFrame = new JFrame("Help Demo");
@@ -36,97 +70,23 @@ class Help {
     jlistTopics = new JList<>(arrayTopics);
     jlistTopics.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
     jlistTopics.addListSelectionListener((e) -> {
-      
+      List<String> selectionList = jlistTopics.getSelectedValuesList();
+      String[] selectionArray = selectionList.toArray(new String[0]);
+
+      if(selectionArray.length > 0) 
+        jlabelTopicText.setText(String.join("<br><br>", selectionArray)); 
+      else // Othewise, reprompt. 
+        jlabelTopicText.setText(""); 
     });
+    
+    jframeFrame.add(jlabelSelectTopics);
+    jframeFrame.add(jlistTopics);
+    jframeFrame.add(jlabelTopicText);
   }
-  
-  
-  void helpOn(int what) {
-    switch(what) {
-      case '1':
-        System.out.println("The if:\n");
-        System.out.println("if(condition) statement;");
-        System.out.println("else statement;");
-        break;
-      case '2':
-        System.out.println("The switch:\n");
-        System.out.println("switch(expression) {");
-        System.out.println("  case constant:");
-        System.out.println("    statement sequence");
-        System.out.println("    break;");
-        System.out.println("  // ...");
-        System.out.println("}");
-        break;
-      case '3':
-        System.out.println("The for:\n");
-        System.out.print("for(init; condition; iteration)");
-        System.out.println(" statement;");
-        break;
-      case '4':
-        System.out.println("The while:\n");
-        System.out.println("while(condition) statement;");
-        break;
-      case '5':
-        System.out.println("The do-while:\n");
-        System.out.println("do {");
-        System.out.println("  statement;");
-        System.out.println("} while (condition);");
-        break;
-      case '6':
-        System.out.println("The break:\n");
-        System.out.println("break; or break label;");
-        break;
-      case '7':
-        System.out.println("The continue:\n");
-        System.out.println("continue; or continue label;");
-        break;
-    }
-    System.out.println();
-  }
-
-  void showMenu() {
-    System.out.println("Help on:");
-    System.out.println("  1. if");
-    System.out.println("  2. switch");
-    System.out.println("  3. for");
-    System.out.println("  4. while");
-    System.out.println("  5. do-while");
-    System.out.println("  6. break");
-    System.out.println("  7. continue\n");
-    System.out.print("Choose one (q to quit): ");
-  }
-
-  boolean isValid(int ch) {
-    if(ch < '1' | ch > '7' & ch != 'q') return false;
-    else return true;
-  }
-
 }
 
 class HelpClassDemo {
-  public static void main(String args[])
-    throws java.io.IOException {
-    char choice, ignore;
-    Help hlpobj = new Help();
-
-    for(;;) {
-      do {
-        hlpobj.showMenu();
-
-        choice = (char) System.in.read(); 
-
-        do { 
-           ignore = (char) System.in.read(); 
-         } while(ignore != '\n'); 
- 
-      } while( !hlpobj.isValid(choice) );
-
-      if(choice == 'q') break;
-
-      System.out.println("\n");
-
-
-      hlpobj.helpOn(choice);
-    }
+  public static void main(String args[]) {
+    SwingUtilities.invokeLater(() -> new Help());
   }
 }
